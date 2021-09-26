dba5e22ddf1b2ee70fa0d857e242607ae537036e
rathoreumang <umang.rathore997@gmail.com>
2021-09-26 12:50:50 +0530

    added number.java

---
+++ number.java
@@ -0,0 +1,8 @@
+public class number{
+    public static void main(String args[]){
+        float a = 5;
+        float b = 10;
+        float c = (a/b)*100;
+        System.out.println("Output: " + c +"%");
+    }
+}
